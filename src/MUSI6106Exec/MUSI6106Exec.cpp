05ee8bfa16dbd69b09546225031df74a6e6b6b10
likelian <likelian@me.com>
2022-01-16 23:03:57 -0500

    mostly?

--- src/MUSI6106Exec/MUSI6106Exec.cpp
+++ src/MUSI6106Exec/MUSI6106Exec.cpp
@@ -41,42 +41,79 @@ int main(int argc, char* argv[])
  
     //////////////////////////////////////////////////////////////////////////////
     // open the input wave file
-    phAudioFile->create(CAudioFileIf*& pCInstance); //only return error,
-    phAudioFile->openFile(sInputFilePath, CAudioFileIf::kFileRead);
+    CAudioFileIf * pCInstance;
+    phAudioFile->create(pCInstance); //only return error
+    pCInstance->openFile(sInputFilePath, CAudioFileIf::kFileRead);
+    
  
     //////////////////////////////////////////////////////////////////////////////
     // open the output text file
-    std::ofstream textFile(sOutputFilePath);
+    std::ofstream textFile;
+    textFile.open(sOutputFilePath, std::ofstream::out | std::ofstream::app);
 
     
     //////////////////////////////////////////////////////////////////////////////
     // allocate memory
-    float* buffer_ptr;
-    buffer_ptr = new float[kBlockSize];
+    float fBuffer_L[kBlockSize];
+    float fBuffer_R[kBlockSize];
 
     
     //////////////////////////////////////////////////////////////////////////////
     // get audio data and write it to the output text file (one column per channel)
     
-    long long int iNumFrames;
-    Error_t data = phAudioFile->readData(ppfAudioData, iNumFrames);
+    
+    long long iLengthInFrames;
+    pCInstance->getLength(iLengthInFrames);
+    //long long int iNumFrames = iLengthInFrames;
     
     
-    for (int i=0; i<=kBlockSize; i++){
-        buffer_ptr[i] = (float)i;
+    long long iNumFrames = 20;
+
+    // allocate ppfAudioData
+    int row = 2,col = iNumFrames;
+    ppfAudioData = (float **) malloc(row*sizeof(float *));
+    for(int i=0;i<iNumFrames;i++)
+        ppfAudioData[i] = (float *) malloc(col*sizeof(int));
+    
+    
+    long long iFrame = 0;
+    
+    cout << iLengthInFrames;
+    
+    while(iFrame<iLengthInFrames-iFrame){
+        
+        cout << iFrame << endl;
+        
+        pCInstance->setPosition(iFrame);
+        
+        cout << iFrame << endl;
+        
+        for (int i=0; i<kBlockSize; i++){
+                  pCInstance->readData(ppfAudioData, iNumFrames);
+                  fBuffer_L[i] = *ppfAudioData[0];
+                  fBuffer_R[i] = *ppfAudioData[1];
+                  
+              }
+              
+        for (int i=0; i<kBlockSize; i++){
+                  textFile << fBuffer_L[i] << ' ' << fBuffer_R[i] << '\n';
+        }
+        
+        pCInstance->getPosition(iFrame);
+        
+        cout << iFrame << endl;
+        
     }
     
-    for (int i=0; i<=kBlockSize; i++){
-        textFile << buffer_ptr[i] << '\n';
-    }
+   
 
 
     //////////////////////////////////////////////////////////////////////////////
     // clean-up (close files and free memory)
-    
-    //phAudioFile->closeFile();
-    delete[] buffer_ptr;
     textFile.close();
+    pCInstance->reset();
+    pCInstance->closeFile();
+    phAudioFile->destroy(pCInstance);
 
     // all done
     return 0;
